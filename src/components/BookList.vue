af454b769c8842d7d39170a2896027f5778d9987
Tom Eagle <tom.eagle@reckless.agency>
2018-10-23 12:42:02 +0100

    npm install, looaded booklist instead of HelloWorld

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{ this.title }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'BookList',
+    data: function(){
+        return {
+            title: "All Books"
+        }
+    }
+}
+</script>
+
+<style>
+    h1, h2 {
+        font-weight: normal;
+    }
+</style>
+
+
